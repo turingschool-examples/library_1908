2a744c9c20b10c2f578779b9c78c82eac8159c9e
Jon Patterson <JonPatt92@gmail.com>
2019-09-04 09:28:13 -0600

    add book array

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,9 @@
 class Author
-
+  attr_reader :books
   def initialize(author_hash)
     @first_name = author_hash[:first_name]
     @last_name = author_hash[:last_name]
+    @books = []
   end
 
   def name
--- test/author_test.rb
+++ test/author_test.rb
@@ -16,4 +16,9 @@ class AuthorTest < Minitest::Test
   def test_it_knows_full_name
     assert_equal "Charlotte Bronte", @charlotte_bronte.name
   end
+
+  def test_it_starts_books_as_empty_array
+    assert_equal [], @charlotte_bronte.books
+  end
+
 end
