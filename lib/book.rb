bd4feb511449a53e7db61282e8d4f94dd9c209fd
David Lopez <davidclopez1@live.com>
2019-09-04 09:11:28 -0600

    Finish iteration 1 and fix

--- lib/book.rb
+++ lib/book.rb
@@ -5,9 +5,6 @@ class Book
   def initialize(info)
     @author = info[:author_first_name] + ' ' + info[:author_last_name]
     @title = info[:title]
-    @publication_date = info[:publication_date]
+    @publication_date = info[:publication_date].split(', ')[1]
   end
-
-
-
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -14,7 +14,6 @@ class BookTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Harper Lee", @book.author
     assert_equal "To Kill a Mockingbird", @book.title
-    assert_equal "July 11, 1960", @book.publication_date
+    assert_equal "1960", @book.publication_date
   end
-
 end
