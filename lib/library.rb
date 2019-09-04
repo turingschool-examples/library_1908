d699785a27729c5b2028e305ee4d69976512e50a
Rachel Lew <rlew07@gmail.com>
2019-09-04 10:40:54 -0600

    Adds add_author method

--- lib/library.rb
+++ lib/library.rb
@@ -1,10 +1,14 @@
 class Library
 
   attr_reader :name, :books, :authors
-  
+
   def initialize(name)
     @name = name
     @books = []
     @authors = []
+  end
+
+  def add_author(author)
+    @authors << author
   end
 end
