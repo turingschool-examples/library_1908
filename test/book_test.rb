a8e0ea29914fd2c818270d1d2a5c331fdc9d1464
John Travers <johnk.travers@gmail.com>
2019-09-04 09:28:09 -0600

    Replace double quotes with single quotes

--- test/book_test.rb
+++ test/book_test.rb
@@ -6,10 +6,10 @@ class BookTest < Minitest::Test
 
   def setup
     @book = Book.new({
-      author_first_name: "Harper",
-      author_last_name: "Lee",
-      title: "To Kill a Mockingbird",
-      publication_date: "July 11, 1960"
+      author_first_name: 'Harper',
+      author_last_name: 'Lee',
+      title: 'To Kill a Mockingbird',
+      publication_date: 'July 11, 1960'
       })
   end
 
