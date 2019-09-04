4f4050043267b46d888c2900f1f858d8667e86fa
sasloan <sebastiansloan94@gmail.com>
2019-09-04 11:00:34 -0600

    Time up

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,20 @@
 class Author
-  attr_reader :first_name, :last_name
+  attr_reader :first_name, :last_name, :books
 
   def initialize(attributes)
     @first_name = attributes[:first_name]
     @last_name = attributes[:last_name]
+    @books = []
   end
+
+  def name
+    @first_name + ' ' + @last_name
+  end
+
+  def write(title, publication_date)
+  @books << Book.new({:title, :author_first_name, :author_last_name, :publication_date, :publication_year})
+    #I want to put out a book by using the title of the book and
+    # publication_date.
+  end
+  # binding.pry
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -2,12 +2,14 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/book'
 require './lib/author'
+require 'pry'
 
 class AuthorTest < Minitest::Test
 
   def setup
     @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-    @jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a mocking bird", publication_date: "July 11, 1960"})
+    @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
   end
 
   def test_it_exists
@@ -18,4 +20,17 @@ class AuthorTest < Minitest::Test
     assert_equal "Charlotte", @charlotte_bronte.first_name
     assert_equal "Bronte", @charlotte_bronte.last_name
   end
+
+  def test_it_has_a_full_name
+    assert_equal "Charlotte Bronte", @charlotte_bronte.name
+  end
+
+  def test_it_can_add_books
+    assert_equal [], @charlotte_bronte.books
+  end
+
+  def test_it_can_write
+    assert_equal @jane_eyre, @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+  end
+  # binding.pry
 end
