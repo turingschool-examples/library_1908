a3fc73410a21258ecd8b8a1c2098c9639df59666
Laura Schulz <lrs8810@gmail.com>
2019-09-04 10:09:30 -0600

    WIP author can write multiple books

--- lib/author.rb
+++ lib/author.rb
@@ -1,12 +1,25 @@
+require './lib/book'
+
 class Author
+  attr_reader :books
 
   def initialize(author_details)
     @first_name = author_details[:first_name]
     @last_name = author_details[:last_name]
+    @books = []
   end
 
   def name
     @first_name + " " + @last_name
   end
 
+  def write(title, publication_date)
+    @books << Book.new({
+    author_first_name: @first_name,
+    author_last_name: @last_name,
+    title: title,
+    publication_date: publication_date
+    })
+    @books.first
+  end
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -15,4 +15,21 @@ class AuthorTest < Minitest::Test
   def test_author_has_name
     assert_equal "Charlotte Bronte", @charlotte_bronte.name
   end
+
+  def test_author_starts_without_books
+    assert_equal [], @charlotte_bronte.books
+  end
+
+  def test_author_can_write_one_book
+    jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    assert_equal Book, jane_eyre.class
+    assert_equal "Jane Eyre", jane_eyre.title
+    assert_equal [jane_eyre], @charlotte_bronte.books
+  end
+
+  def test_author_can_write_multiple_books
+    jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    villette = @charlotte_bronte.write("Villette", "1853")
+    assert_equal [jane_eyre, villette], @charlotte_bronte.books
+  end
 end
